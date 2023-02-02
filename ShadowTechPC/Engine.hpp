3bcd8533e5c71426d6bd772f08257ee598fa07db
vition vlad <48290199+VitionVlad@users.noreply.github.com>
2023-02-02 18:01:31 +0200

    Update Engine.hpp

--- ShadowTechPC/Engine.hpp
+++ ShadowTechPC/Engine.hpp
@@ -213,6 +213,8 @@ class Engine{
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+        float borderColor[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+        glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor); 
         glBindTexture(GL_TEXTURE_2D, 0);
 
         glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, shadowimg, 0);
